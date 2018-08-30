1fe6d8ee38df4bb0df41b6b1ac5131f23d9f2303
Dawn <icy19882006@163.com>
2018-08-30 14:35:21 +0800

    注释

--- ExtentionDemo/UIKit/UIButton+Position.h
+++ ExtentionDemo/UIKit/UIButton+Position.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+
+/*
+ 图片的上下坐相对于按钮，文字上下右相对于按钮，文字左面相对于图片右面
+ 配置不同的对齐方式时
+ 如：上+左
+ 判断图片和文字上边距和左边距移动的相对距离
+ 如：中心
+ 判断图片和文字中心点移动的相对距离
+*/
+
 typedef enum {
     ButtonImagePosition_Left,
     ButtonImagePosition_right,
